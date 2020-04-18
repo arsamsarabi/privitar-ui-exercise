b2c7a3b7356736bbaf941bdd23eb459a172806de
Arsam Sarabi <arsamsarabi@me.com>
2020-04-18 16:02:18 +0100

    ✅ person.test updated

--- src/store/person/person.test.ts
+++ src/store/person/person.test.ts
@@ -3,12 +3,14 @@ import Person, { IPersonStore } from "./person";
 describe("Person Store", () => {
   it("Should initialize correctly", () => {
     const isabelle: IPersonStore = new Person({
+      id: 2020,
       firstName: "Isabelle",
       lastName: "Ringing",
       nationality: "UK",
       age: 32,
       riskPercentage: 42,
     });
+    expect(isabelle.id).toEqual(2020);
     expect(isabelle.firstName).toEqual("Isabelle");
     expect(isabelle.lastName).toEqual("Ringing");
     expect(isabelle.fullName).toEqual("Isabelle Ringing");
