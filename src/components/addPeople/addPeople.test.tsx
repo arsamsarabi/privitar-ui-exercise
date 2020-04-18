540533023730b0de329089e1fe16328193d6a789
Arsam Sarabi <arsamsarabi@me.com>
2020-04-18 23:43:05 +0100

    🚧 addPeople tests

--- src/components/addPeople/addPeople.test.tsx
+++ src/components/addPeople/addPeople.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import {
   StyledAddPeople,
@@ -7,10 +7,32 @@ import {
   StyledButton,
 } from "./StyledAddPeople";
 import { AddPeople } from "./AddPeople";
+import { theme } from "../../styles";
+
+describe("AddPeople", () => {
+  const handleSubmit = jest.fn();
+  it("Should render as expected", () => {
+    const component = mount(<AddPeople handleSubmit={handleSubmit} />);
+  });
+});
 
 describe("StyledAddPeople", () => {
   it("Should render as expected", () => {
     const component = shallow(<StyledAddPeople />);
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("StyledTextArea", () => {
+  it("Should render as expected", () => {
+    const component = shallow(<StyledTextArea theme={theme} />);
+    expect(component).toMatchSnapshot();
+  });
+});
+
+describe("StyledButton", () => {
+  it("Should render as expected", () => {
+    const component = shallow(<StyledButton theme={theme} />);
     expect(component).toMatchSnapshot();
   });
 });
